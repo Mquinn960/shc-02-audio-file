4108f8616cc70bded0a082c223d8bac5a859774e
Matthew Quinn <matt@mquinn.co.uk>
2021-06-08 22:25:36 +0100

    main headers compiled

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod header;
 
 use std::io::{BufWriter, Write};
 use std::fs::{OpenOptions, File};
+use std::convert::TryFrom;
 
 use byteorder::{LittleEndian, WriteBytesExt};
+use dasp_signal::{self as signal, Signal};
 
 use header::riff_header::RiffHeader;
 use header::fmt_header::FmtHeader;
@@ -24,15 +26,23 @@ fn main() {
         .open("./test.wav")
         .expect("Unable to open file");
     let mut f = BufWriter::new(f);
+
+    // Actual data
+    let mut data = Vec::new();
+    let mut i = 0;
+    while i < 100000 {
+        data.write_u16::<LittleEndian>(43000).unwrap();
+        i += 1;
+    }
     
-    // Fill in overall file size, size of data section
-    // header file size = total - 8
-    // data file size = total - 44
-    
+    let data_len = data.len();
+    let data_size = u32::try_from(data_len).unwrap();
+
     // RIFF Header
     f.write_all(&riff_header.chunk_id).expect("Unable to write data");
 
     let mut wtr = Vec::new();
+    riff_header.chunk_size = data_size - 8;
     wtr.write_u32::<LittleEndian>(riff_header.chunk_size).unwrap();
     f.write_all(&wtr).expect("Unable to write data");
 
@@ -55,18 +65,11 @@ fn main() {
     f.write_all(&data_header.subchunk_2_id).expect("Unable to write data");
 
     let mut wtr = Vec::new();
-    wtr.write_u32::<LittleEndian>(data_header.subchunk_2_size).unwrap();
+    data_header.subchunk_2_size = data_size - 44;
     wtr.write_u32::<LittleEndian>(data_header.subchunk_2_size).unwrap();
     f.write_all(&wtr).expect("Unable to write data");
 
-    // Actual data
-    let mut wtr = Vec::new();
-    
-    let i = 0;
-    while i < 100000 {
-        wtr.write_u16::<LittleEndian>(23).unwrap();
-    }
-    f.write_all(&wtr).expect("Unable to write data");
+    f.write_all(&data).expect("Unable to write data");
 
     println!("Finished!");
 }
